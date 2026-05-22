a2cd716ada4e4d408e759f31b803c7e7e5754e83
skygori <0137020@gmail.com>
2026-05-22 11:17:39 +0900

    branch_folder
    branch test

---
+++ F303/nucleo_F303RE_03_ex/App/ap.h
@@ -0,0 +1,17 @@
+/*
+ * ap.h
+ *
+ *  Created on: 2026. 5. 22.
+ *      Author: ljy
+ */
+
+#ifndef AP_H_
+#define AP_H_
+
+
+#include "def.h"
+
+void apInit(void);
+void apMain(void);
+
+#endif /* AP_H_ */
