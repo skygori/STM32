186fd85822c2b09da91cd78021232855a6af5d26
skygori <0137020@gmail.com>
2026-05-26 10:08:32 +0900

    add uart
    uart test 1

--- F303/nucleo_F303RE_03_ex/App/ap.c
+++ F303/nucleo_F303RE_03_ex/App/ap.c
@@ -8,6 +8,10 @@
 
 #include "ap.h"
 
+#include "usart.h"
+
+uint32_t cnt = 0;
+
 void apInit(void)
 {
 	ledInit();
@@ -16,8 +20,17 @@ void apInit(void)
 void apMain(void)
 {
   uint32_t pre_time;
+  uint32_t pre_time2;
 
   pre_time = millis();
+  pre_time2 = millis();
+
+  uint8_t buffer[256];
+
+  sprintf((char *)buffer, "Firmware Start\r\n");
+  HAL_UART_Transmit(&huart2, buffer, strlen((char *)buffer), 100);
+
+  HAL_StatusTypeDef serial_status; // uart 상태 저장
 
 	while(1)
 	{
@@ -30,6 +43,21 @@ void apMain(void)
 	    pre_time = millis();
 
 	    ledToggle(RUN_LED);
+	  }
+
+//	  if(millis() - pre_time2 >= 1000)
+//    {
+//      pre_time2 = millis();
+//
+//
+//
+//      sprintf((char *)buffer, "TX test cnt : %d\r\n", cnt++);
+//      HAL_UART_Transmit(&huart2, buffer, strlen((char *)buffer), 100);
+//    }
+	  serial_status = HAL_UART_Receive(&huart2, buffer, 1, 100);
+	  if(serial_status == HAL_OK)
+	  {
+	    HAL_UART_Transmit(&huart2, buffer, 1, 100);
 	  }
 
 	  //delay(500);
